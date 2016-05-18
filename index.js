e9232be4a6c2aee1313440e539bb75fac08f2264
Victor Hernandes Silva Maia <srvictormaia@gmail.com>
2016-05-17 23:35:15 -0300

    dunno

---
+++ index.js
@@ -0,0 +1,13 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+window.onload = function(){
+    ReactDOM.render(
+        React.DOM.p({}, "test"),
+        document.getElementById('test'));
+};
+
+
+
+
+
